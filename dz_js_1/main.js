1cdf0fd01ac8abf689fa21be468c8d3e22f4da85
Denis Putiy <dp@se7ensky.com>
2017-11-29 23:03:28 +0200

    Added dz Js 1

---
+++ dz_js_1/main.js
@@ -0,0 +1,105 @@
+var arraySchool = [
+					{ parallel1:
+						{
+							classA: {
+								human1: {
+									post: "teacher",
+									name: "Anna"
+								},
+
+								human2: {
+									post: "student",
+									name: "Dennis"
+								},
+
+								human3: {
+									post: "student",
+									name: "Dima"
+								},
+
+								human4: {
+									post: "student",
+									name: "Serjio"
+								},
+							},
+
+							classB: {
+							human1: {
+								post: "teacher",
+								name: "Irinna"
+							},
+
+							human2: {
+								post: "student",
+								name: "Alex"
+							},
+
+							human3: {
+								post: "student",
+								name: "Egor"
+							},
+
+							human4: {
+								post: "student",
+								name: "Roma"
+							},
+						},
+						}
+					},
+					{ parallel2:
+						{
+							classA: {
+								human1: {
+									post: "teacher",
+									name: "Julia"
+								},
+
+								human2: {
+									post: "student",
+									name: "Stiv"
+								},
+
+								human3: {
+									post: "student",
+									name: "Scott"
+								},
+
+								human4: {
+									post: "student",
+									name: "Serjii"
+								},
+							},
+
+							classB: {
+								human1: {
+									post: "teacher",
+									name: "Victoria"
+								},
+
+								human2: {
+									post: "student",
+									name: "Victor"
+								},
+
+								human3: {
+									post: "student",
+									name: "Anrusha"
+								},
+
+								human4: {
+									post: "student",
+									name: "Sasha"
+								},
+							},
+						}
+					}
+				];
+
+console.log(arraySchool);
+
+
+var subtraction= function(num1, num2){
+	return  num1-num2;
+};
+
+console.log(subtraction(60,20));
