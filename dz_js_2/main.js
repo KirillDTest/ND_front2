f2069840d19dc461c4dcb14e962ba841ff218ccd
Denis Putiy <dp@se7ensky.com>
2017-12-01 17:14:45 +0200

    added 2 variant

--- dz_js_2/main.js
+++ dz_js_2/main.js
@@ -1,16 +1,38 @@
-var a= 80;
-var b= 1.73;
-var BMI= a/(b*b);
+// variant1
 
-if (BMI < 18.5){
-	console.log("недостаточная масса")
-};
-if (18.5 <= BMI && BMI < 25.0){
-	console.log("норма")
-};
-if (25.0 <= BMI && BMI < 30.0){
-	console.log("избыточная масса")
-};
-if (30.0 <= BMI){
-	console.log("ожирение")
-};
+// var a= 80;
+// var b= 1.73;
+// var BMI= a/(b*b);
+//
+// if (BMI < 18.5){
+// 	console.log("недостаточная масса")
+// };
+// if (18.5 <= BMI && BMI < 25.0){
+// 	console.log("норма")
+// };
+// if (25.0 <= BMI && BMI < 30.0){
+// 	console.log("избыточная масса")
+// };
+// if (30.0 <= BMI){
+// 	console.log("ожирение")
+// };
+
+// variant 2
+
+var a = prompt('Введите ваш вес','80');
+var b = prompt('Введите ваш рост','1.73');
+
+function BMI(a,b) {
+	var result = a/(b*b);
+	return result;
+}
+
+if (BMI(a,b) < 18.5) {
+	alert('недостаточная масса');
+} else if (18.5 <= BMI(a,b) && BMI(a,b) < 25.0) {
+	alert('норма');
+} else  if (25.0 <= BMI(a,b) && BMI(a,b) < 30.0){
+	alert('избыточная масса');
+} else {
+	alert('ожирение');
+}
