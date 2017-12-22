99af642fb62b7d5a481a20adc79ddb67ea6f07a0
ES <evgeniy.saschenko@gmail.com>
2017-12-22 16:21:49 +0200

    slice

--- 29/app.js
+++ 29/app.js
@@ -1,7 +1,7 @@
 var str= '123456789';
 
 var fun= ( str, num )=>{
-	return str.slice( num, str.length -1 ) + str.slice( 0, num );
+	return str.slice( num, str.length ) + str.slice( 0, num );
 }
 
 console.log( fun(str, 3) )
