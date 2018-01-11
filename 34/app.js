42cbd46cdc7a417168900624aa28ef9e1b258b9b
ES <evgeniy.saschenko@gmail.com>
2018-01-11 17:01:04 +0200

    modal

---
+++ 34/app.js
@@ -0,0 +1,14 @@
+btn.addEventListener('click', (e)=>{
+	modal.classList.toggle('visible');
+	message.classList.toggle('visible');
+})
+
+clo.addEventListener('click', (e)=>{
+	modal.classList.toggle('visible');
+	message.classList.toggle('visible');
+})
+
+modal.addEventListener('click', (e)=>{
+	modal.classList.toggle('visible');
+	message.classList.toggle('visible');
+})
